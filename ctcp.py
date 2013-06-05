a5aa2af70d871ed6bad14255df3e8ebb3e65d3e7
HarHar <harhar-captain@live.com>
2013-06-05 15:01:52 -0300

    Add modules from the main repository

---
+++ ctcp.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class BotModule(object):
+	def __init__(self, storage):
+		self.storage = storage
+		self.admins = {}
+		self.bot = None
+
+		self.pending = {}
+	def register(self):
+		return {'functions': [{'ctcp': self.cmd_ctcp}, {'priv': self.cmd_privset}]}
+	def event(self, ev):
+		if ev['name'] == 'msg':
+			if ev['msg'][0] == chr(1) and ev['from'].nick.lower() in self.pending:
+				split = ev['msg'].replace(chr(1), '').split(' ')
+				x = ''
+				for w in split[1:]:
+					x += w + ' '
+				x = x[:-1]
+				self.bot.msg(self.pending[ev['from'].nick.lower()], 'CTCP ' + chr(2) + split[0] + chr(15) + ' reply from ' + ev['from'].nick + ': ' + x)
+				del self.pending[ev['from'].nick.lower()]
+
+	def cmd_ctcp(self, args, receiver, sender):
+		"""ctcp [nick] [type] | {'public': True, 'admin_only': False} | Sends a ctcp to the desired nick and shows return"""
+		s = args.split(' ')
+		if len(s) != 2:
+			receiver.msg(chr(2) + 'Usage:' + chr(15) + ' ' + self.bot.cmd_char + 'ctcp [nick] [type]')
+			return
+
+		if s[0].lower() in self.pending:
+			receiver.msg(chr(3) + '05Error' + chr(15) + ' already waiting for CTCP reply from user')
+			return
+
+		self.bot.msg(s[0], chr(1) + s[1].upper() + ' ' + chr(1))
+		self.pending[s[0].lower()] = receiver.name
+
+	def cmd_privset(self, args, receiver, sender):
+		"""priv [on/off] | {'public': False, 'admin_only': False} | Turns CTCP privacy on or off"""
+		receiver.msg('404 Not Implemented')
